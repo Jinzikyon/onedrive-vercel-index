524aeb33a563a8af1bfb70ac2baff37418e0cd6b
Jinzikyon <74198729+Jinzikyon@users.noreply.github.com>
2022-05-18 22:13:34 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/favicon.ico',
+  icon: '/icons/favicon.ico',
 
   // The name of your website. Present alongside your icon.
   title: "JIN的资源库",
@@ -48,6 +48,16 @@ module.exports = {
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
 
+    links: [
+    {
+      name: 'GitHub',
+      link: 'https://github.com/jinzikyon/onedrive-vercel-index',
+    },
+    {
+      name: 'Telegram',
+      link: 'https://t.me/realSpencerWoo',
+    },
+  ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
