4ada9a42d4f56295a2c17c82f02140765b16b40e
Jinzikyon <74198729+Jinzikyon@users.noreply.github.com>
2022-05-18 21:58:23 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "JIN的视频库",
+  title: "JIN的资源库",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
