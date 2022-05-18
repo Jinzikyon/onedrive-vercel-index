0e2beff9dd3978da11ad80496dd0158ea84057a6
Jinzikyon <74198729+Jinzikyon@users.noreply.github.com>
2022-05-18 21:58:07 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -7,14 +7,14 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
   // your email being exposed in public.
-    userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || '5T0216@r18.nl',
+    userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'anime@sanae.ga',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "JIN的OneDrive",
+  title: "JIN的视频库",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/u-need-a-password'],
+  protectedRoutes: ['/个人库'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'jin0@sanae.ga',
@@ -47,16 +47,7 @@ module.exports = {
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-  links: [
-    {
-      name: 'GitHub',
-      link: 'https://github.com/jinzikyon/onedrive-vercel-index',
-    },
-    {
-      name: 'Telegram',
-      link: 'https://t.me/000',
-    },
-  ],
+
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
